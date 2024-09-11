d5d1c4ee0f368d9ec22f4321e536a5e9f4d5e27a
AminGhasempoor <aminghasempoor79@gmail.com>
2024-09-11 11:18:44 +0330

    admin dashboard lay out

---
+++ src/lib/NavigationLinks.ts
@@ -0,0 +1,18 @@
+export const Links = [
+    {
+        name : "Dashboard",
+        href : "/dashboard",
+    },
+    {
+        name : "Orders",
+        href : "/dashboard/orders",
+    },
+    {
+        name : "Products",
+        href : "/dashboard/products",
+    },
+    {
+        name : "Categories",
+        href : "/dashboard/categories",
+    },
+]
