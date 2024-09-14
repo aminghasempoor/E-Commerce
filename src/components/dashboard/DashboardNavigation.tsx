0b07dcd86dd70545b888d761d775ab4a94bd7f75
AminGhasempoor <aminghasempoor79@gmail.com>
2024-09-14 13:05:20 +0330

    Products page

--- src/components/dashboard/DashboardNavigation.tsx
+++ src/components/dashboard/DashboardNavigation.tsx
@@ -1,12 +1,16 @@
+"use client"
 import Link from "next/link";
 import {Links} from "@/lib/NavigationLinks";
+import {usePathname} from "next/navigation";
+import {cn} from "@/lib/utils";
 
 const DashboardNavigation = () => {
+    const pathName = usePathname()
     return(
         <>
             {Links.map((link)=>{
                 return(
-                    <Link key={link.href} href={link.href}>
+                    <Link key={link.href} href={link.href} className={cn(link.href === pathName ? "text-foreground border-b" : "text-muted-foreground hover:text-foreground")}>
                         {link.name}
                     </Link>
                 )
