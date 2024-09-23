a5f6d4e6742eafa397c5b504b8b7100a376cd0ff
AminGhasempoor <aminghasempoor79@gmail.com>
2024-09-23 14:09:21 +0330

    poduct id page

--- src/app/action.ts
+++ src/app/action.ts
@@ -37,3 +37,42 @@ export async function CreateProduct(prevState : unknown,formData : FormData){
     })
     return redirect("/dashboard")
 }
+
+export async function editProduct(prevState: never, formData: FormData) {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
+
+    if (!user) {
+        return redirect("/");
+    }
+
+    const submission = parseWithZod(formData, {
+        schema: productSchema,
+    });
+
+    if (submission.status !== "success") {
+        return submission.reply();
+    }
+
+    const flattenUrls = submission.value.images.flatMap((urlString) =>
+        urlString.split(",").map((url) => url.trim())
+    );
+
+    const productId = formData.get("productId") as string;
+    await prisma.product.update({
+        where: {
+            id: productId,
+        },
+        data: {
+            name: submission.value.name,
+            description: submission.value.description,
+            category: submission.value.category,
+            price: submission.value.price,
+            isFeatured: submission.value.isFeatured === true ? true : false,
+            status: submission.value.status,
+            images: flattenUrls,
+        },
+    });
+
+    redirect("/dashboard/products");
+}
